772f832201dde6ff8529876a7bf1176a0e7ceedf
Yurii Motsnyi <y.motsnyy@gmail.com>
2023-01-02 18:12:20 +0200

    modify

--- HW JS _B  #5/dist/script.dev.js
+++ HW JS _B  #5/dist/script.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var newUser1 = createNewUser();
+console.log(newUser1);
 console.log(newUser1.getAge());
 console.log(newUser1.getLogin());
 console.log(newUser1.getPassword());
@@ -12,18 +13,18 @@ function createNewUser() {
   newUser.birthday = prompt("Enter your birthday date", "dd.mm.yyyy");
 
   newUser.getLogin = function () {
-    return "".concat(this.userFirstName[0]).concat(this.userLastName).toLowerCase();
+    return "Login: ".concat(this.userFirstName[0].toLowerCase()).concat(this.userLastName.toLowerCase());
   };
 
   newUser.getAge = function () {
     var dob = this.birthday.split(".").reverse();
     var ageMs = Date.now() - new Date(dob).getTime();
     var ageDt = new Date(ageMs);
-    return ageDt.getFullYear() - 1970;
+    return "Age: ".concat(ageDt.getFullYear() - 1970);
   };
 
   newUser.getPassword = function () {
-    return this.userFirstName[0].toUpperCase() + this.userLastName.toLowerCase() + this.birthday.slice(-4);
+    return "Password: ".concat(this.userFirstName[0].toUpperCase()).concat(this.userLastName.toLowerCase()).concat(this.birthday.slice(-4));
   };
 
   return newUser;
--- HW JS _B  #5/script.js
+++ HW JS _B  #5/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const newUser1 = createNewUser();
+console.log(newUser1);
 console.log(newUser1.getAge());
 console.log(newUser1.getLogin());
 console.log(newUser1.getPassword());
@@ -12,19 +13,16 @@ function createNewUser() {
   newUser.userLastName = prompt("Enter your last name", "");
   newUser.birthday = prompt("Enter your birthday date", "dd.mm.yyyy");
   newUser.getLogin = function () {
-    return `${this.userFirstName[0]}${this.userLastName}`.toLowerCase();
+    return `Login: ${this.userFirstName[0].toLowerCase()}${this.userLastName.toLowerCase()}`;
   };
   newUser.getAge = function () {
       const dob = this.birthday.split(".").reverse();
       const ageMs = Date.now() - new Date(dob).getTime();
       const ageDt = new Date(ageMs);
-      return ageDt.getFullYear() - 1970;
+      return `Age: ${ageDt.getFullYear() - 1970}`;
     };
   newUser.getPassword = function () {
-      return (
-        this.userFirstName[0].toUpperCase() +
-        this.userLastName.toLowerCase() +
-        this.birthday.slice(-4)
+      return (`Password: ${this.userFirstName[0].toUpperCase()}${this.userLastName.toLowerCase()}${this.birthday.slice(-4)}`
       );
     };
   return newUser;
